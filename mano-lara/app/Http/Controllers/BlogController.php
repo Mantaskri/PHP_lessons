5e7c2923b72356e71ec073934eca4704d0176996
Mantas Krinickas <mantas.krinickas@hotmail.com>
2022-09-26 09:19:06 +0300

    Blog model with controller

--- mano-lara/app/Http/Controllers/BlogController.php
+++ mano-lara/app/Http/Controllers/BlogController.php
@@ -21,7 +21,7 @@ class BlogController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
+     *$
      * @return \Illuminate\Http\Response
      */
     public function create()
