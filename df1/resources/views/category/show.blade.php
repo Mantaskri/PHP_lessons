930b1c31082d73d367416682afcbaffa288b30ee
Mantas Krinickas <mantas.krinickas@hotmail.com>
2022-09-29 14:10:11 +0300

    edit/delete

--- df1/resources/views/category/show.blade.php
+++ df1/resources/views/category/show.blade.php
@@ -10,23 +10,22 @@
                     </div>
                     <div class="card-body">
                         <div class="category">
-                            <h5>{{ $category->name }}</h5>
-                            <h5>{{ $category->surname }}</h5>
+                            <h5>{{ $category->title }}</h5>
                         </div>
-                        <ul class="list-group">
-                            @forelse($category->getTrucks as $truck)
-                                <li class="list-group-item">
-                                    <div class="trucks-list">
-                                        <div class="content">
-                                            <h2><span>plate: </span>{{ $truck->plate }}</h2>
-                                            <h4><span>maker: </span>{{ $truck->maker }}</h4>
-                                        </div>
-                                    </div>
-                                </li>
-                            @empty
-                                <li class="list-group-item">No trucks found</li>
-                            @endforelse
-                        </ul>
+                        {{-- <ul class="list-group">
+                        @forelse($category->getTrucks as $truck)
+                        <li class="list-group-item">
+                            <div class="trucks-list">
+                                <div class="content">
+                                    <h2><span>plate: </span>{{$truck->plate}}</h2>
+                                    <h4><span>maker: </span>{{$truck->maker}}</h4>
+                                </div>
+                            </div>
+                        </li>
+                        @empty
+                        <li class="list-group-item">No trucks found</li>
+                        @endforelse
+                    </ul> --}}
                     </div>
                 </div>
             </div>
